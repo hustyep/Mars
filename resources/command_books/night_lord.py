7a9f1451e671b4c0f9085ffb1c8c263c07f0796b
Eden(严璞) <eden@micous.com>
2023-08-12 14:28:29 +0800

    [update] nl下跳

--- resources/command_books/night_lord.py
+++ resources/command_books/night_lord.py
@@ -11,7 +11,7 @@ from src.common.vkeys import press, key_down, key_up, releaseAll, press_acc
 class Key:
     # Movement
     JUMP = 's'
-    FLASH_JUMP = 's'
+    FLASH_JUMP = ';'
     SHADOW_LEAP = 'a'
     SHADOW_SURGE = 'g'
     ROPE_LIFT = 'b'
@@ -39,7 +39,7 @@ class Key:
     ARACHNID = 'q'
     SHURIKEN = 'c'
     DARK_FLARE = 'w'
-    ERDA_SHOWER = '`'
+    ERDA_SHOWER = '`' 
 
 
 #########################
@@ -59,7 +59,8 @@ def step(direction, target):
     # if d_y > settings.move_tolerance * 1.5:
     if direction == 'down':
             # print(f"step_down: {d_y}")
-            press_acc(Key.JUMP, 1, down_time=0.2,up_time=0.1)
+            if d_y > settings.move_tolerance:
+                press_acc(Key.JUMP, 1, down_time=0.2,up_time=0.5)
             return
     elif direction == 'up':
         # print(f"step_up: {d_y}")
@@ -120,21 +121,91 @@ class Adjust(Command):
                     else:
                         key_down('down')
                         time.sleep(0.05)
-                        press(Key.JUMP, 3, down_time=0.1)
+                        press(Key.JUMP, 1, down_time=0.2, down_time=0.5)
                         key_up('down')
                         time.sleep(0.05)
                     counter -= 1
             error = utils.distance(config.player_pos, self.target)
             toggle = not toggle
+            
+class Move(Command):
+    """Moves to a given position using the shortest path based on the current Layout."""
 
+    def __init__(self, x, y, max_steps=15):
+        super().__init__(locals())
+        self.target = (float(x), float(y))
+        self.max_steps = settings.validate_nonnegative_int(max_steps)
+        self.prev_direction = ''
+
+    def _new_direction(self, new):
+        if new != 'up':
+            # 谨慎按上方向键
+            key_down(new)
+        if self.prev_direction and self.prev_direction != new:
+            key_up(self.prev_direction)
+        self.prev_direction = new
+
+    def main(self):
+        counter = self.max_steps
+        path = config.layout.shortest_path(config.player_pos, self.target)
+        for i, point in enumerate(path):
+            toggle = True
+            self.prev_direction = ''
+            local_error = utils.distance(config.player_pos, point)
+            global_error = utils.distance(config.player_pos, self.target)
+            
+            if global_error <= settings.move_tolerance:
+                break
+            
+            while config.enabled and counter > 0 and \
+                    local_error > settings.move_tolerance and \
+                    global_error > settings.move_tolerance:
+                if toggle:
+                    d_x = point[0] - config.player_pos[0]
+                    if abs(d_x) > settings.move_tolerance / math.sqrt(2):
+                        if d_x < 0:
+                            key = 'left'
+                        else:
+                            key = 'right'
+                        self._new_direction(key)
+                        step(key, point)
+                        if settings.record_layout:
+                            config.layout.add(*config.player_pos)
+                        counter -= 1
+                        if i < len(path) - 1:
+                            time.sleep(0.15)
+                else:
+                    global_d_y = self.target[1] - config.player_pos[1]
+                    d_y = point[1] - config.player_pos[1]
+                    if abs(global_d_y) > settings.move_tolerance / math.sqrt(2) and \
+                        abs(d_y) > settings.move_tolerance / math.sqrt(2):
+                        if d_y < 0:
+                            key = 'up'
+                        else:
+                            key = 'down'
+                        # print(f"move down: {local_error} | {global_error}" )
+                        self._new_direction(key)
+                        step(key, point)
+                        if settings.record_layout:
+                            config.layout.add(*config.player_pos)
+                        counter -= 1
+                        if i < len(path) - 1:
+                            time.sleep(0.05)
+                local_error = utils.distance(config.player_pos, point)
+                global_error = utils.distance(config.player_pos, self.target)
+                toggle = not toggle
+            if self.prev_direction:
+                key_up(self.prev_direction)
+
+# y轴移动
 class MoveUp(Command):
     def __init__(self, dy: float = 0.2):
         super().__init__(locals())
         self.dy = abs(dy)
         
     def main(self):
-        if self.dy <= 0.09:
-            ShadowLeap(True if self.dy >= 0.07 else False).execute()
+        if self.dy <= 0.08:
+            ShadowLeap(True if self.dy >= 0.065 else False).execute()
         else:
             RopeLift(self.dy).execute()
         
@@ -193,6 +264,8 @@ class RopeLift(Command):
         if self.dy >= 0.3:
             press(Key.JUMP, up_time=0.06)
         press_acc(self.__class__.key, up_time=self.dy * 12)
+        if self.dy >= 0.3:
+            time.sleep(self.dy * 10)
         # if self.cancel is not None:
         #     time.sleep(self.cancel)
         #     press(self.__class__.key)
